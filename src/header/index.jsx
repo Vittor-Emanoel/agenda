9181be6715e1abc1a31e5af88b8b299aeeedf1a6
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-01-31 11:05:12 -0300

    header create

--- src/header/index.jsx
+++ src/header/index.jsx
@@ -1,14 +1,20 @@
+import { Plus } from 'phosphor-react'
+
 function Header() {
   return (
-    <header className="">
-      <div>
-        <h1>MyAgenda</h1>
+    <header className="w-full flex justify-center items-center flex-col mt-12 border border-sky-500">
+      <div className="flex w-1/3 justify-between gap-12 mt-8">
+        <h className="font-extrabold text-4xl">MyAgenda</h>
         <button>
           <Plus size={32} />
         </button>
       </div>
-      <div>
-        <input type="text" className="bg-slate-200" />
+      <div className="w-full flex justify-center">
+        <input
+          type="text"
+          placeholder="Qual cliente?"
+          className="w-1/3 p-2 mt-5 mb-5 bg-slate-100"
+        />
       </div>
     </header>
   )
