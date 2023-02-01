16906be2ec04444e83bcdd204037baa3a7ea47dd
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-01 02:00:43 -0300

    add routes and create new form

--- src/components/cliente/index.jsx
+++ src/components/cliente/index.jsx
@@ -1,8 +1,6 @@
-import { Trash } from 'phosphor-react'
-
 function Cliente({ nome, data }) {
   return (
-    <div className="w-full h-16 flex flex-col justify-center p-3 border text-gray-600">
+    <div className="w-full h-16 flex flex-col justify-center p-3  text-gray-600 border">
       <div className="flex justify-between font-bold">
         <h2>NOME</h2>
         <h2>DIA/HORA</h2>
@@ -10,11 +8,6 @@ function Cliente({ nome, data }) {
       <div className="flex justify-between font-light ">
         <div>{nome}</div>
         <div>{data}</div>
-      </div>
-      <div className="flex">
-        <button className="border">
-          <Trash size={32} />
-        </button>
       </div>
     </div>
   )
